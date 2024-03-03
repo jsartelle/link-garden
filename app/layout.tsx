8508738e95b0672c6cff3a6bdaa80eb244cf26a9
James Sartelle <james.sartelle@extra.app>
2024-03-03 11:11:29 -0600

    Favicon support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,28 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.scss'
-export { default as metadata } from '@/config/metadata.json'
+import config from '@/config/config.json'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const icon = require(`@/config/images/${
+  config.metadata.icon ?? 'leaf.svg'
+}`).default
+const appleIcon = require(`@/config/images/${
+  config.metadata.appleIconPNG ?? 'leaf.png'
+}`).default
+
+export const metadata: Metadata = {
+  ...config.metadata,
+  icons: {
+    icon: {
+      url: icon.src,
+    },
+    apple: {
+      url: appleIcon.src,
+    },
+  },
+}
 
 export default function RootLayout({
   children,
